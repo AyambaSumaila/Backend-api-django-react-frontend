1c2b2165db7abd19bcafd5f1acc83e029e789f90
AyambaSumaila <sumailaayamba15@gmail.com>
2024-04-23 18:53:56 +0000

    "Files updated"

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework import generics   # type: ignore
+from .models import Todo 
+from .serializers import TodoSerializer 
 # Create your views here.
+
+#LIst to todo view 
+class ListTodo(generics.ListAPIView):
+    queryset=Todo.objects.all()
+    serializer_class=TodoSerializer
+
+    #Detail Todo view
+class DetailTodo(generics.ListAPIView):
+    queryset=Todo.objects.all()
+    serializer_class=TodoSerializer
+
+
